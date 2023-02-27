061eab79a5ca9a2f541e1ae96e1703e864001a9a
valtimperiumgit <valtimperium@gmail.com>
2023-02-27 14:06:17 +0200

    init

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="login">
+      Login
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Login from './components/login/Login';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
